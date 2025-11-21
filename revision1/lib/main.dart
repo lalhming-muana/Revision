4b99675dac448ff9e5679906efeb5fe7a1ce66c4
lhmuana <edd_vai@yahoo.co.in>
2025-11-21 10:40:21 +0530

    background color is changed

--- revision1/lib/main.dart
+++ revision1/lib/main.dart
@@ -3,7 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(body: Center(child: Text('data'))),
+      home: Scaffold(body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+                    const Color.fromARGB(255, 146, 105, 216),
+                    const Color.fromARGB(255, 74, 24, 160),
+            ],
+        ),
+        ),
+        child: Center(child: Text('data'),),),
     ),
+  ),
   );
+  
 }
